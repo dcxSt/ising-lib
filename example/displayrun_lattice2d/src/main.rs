e1064ed0446561d0300d1146526b3b5d0dbb2983
Stephen Fay <dcxstephen@live.co.uk>
2022-03-06 13:10:35 -0500

    commented example

--- example/displayrun_lattice2d/src/main.rs
+++ example/displayrun_lattice2d/src/main.rs
@@ -2,14 +2,14 @@ use ising_lib::lattice2d::Lattice2d;
 use std::{thread, time};
 
 fn main() {
-    let sixteen_millis = time::Duration::from_millis(7); // wait 16 millis for 60fps
-    let mut lattice = Lattice2d::new_basic([45,145]);
+    let seven_millis = time::Duration::from_millis(7);  // wait 7 millis to slow simulation
+    let mut lattice = Lattice2d::new_basic([45,145]);   // create a lattice
     lattice.disp_terminal();
-    for _ in 0..1200 {
+    for _ in 0..1200 {                  // 1200 frames 
         for _ in 0..2000 {
-            lattice.update();
+            lattice.update();           // update lattice 2000 times between each frame
         }
-        thread::sleep(sixteen_millis); // wait 16 millis for 60 fps
-        lattice.disp_terminal();
+        thread::sleep(seven_millis);    // wait 7 millis for smooth flip-card video display
+        lattice.disp_terminal();        // display the lattice in terminal
     }
 }
