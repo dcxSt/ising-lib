7c1a285b93080676af98d0ba389a2c38b804d3ce
Stephen Fay <dcxstephen@live.co.uk>
2022-04-02 09:37:02 -0400

    tidy up measurements file

--- src/measurement.rs
+++ src/measurement.rs
@@ -5,9 +5,11 @@ use crate::lattice2d::*;
 
 /// The measurement trait measures quantities across different graphs
 trait Measurement {
-    fn get_spin_sum(&self) -> i32; // get the sum of the spin values
-    fn get_spin_expected_value(&self) -> f64; // get the expected value of the spin, i.e. the magnetization per spin 
-    fn measure_energy(&self) -> f64; // energy of the lattice
+    fn get_spin_sum(&self) -> i32;              // get the sum of the spin values
+    fn get_spin_expected_value(&self) -> f64;   // get the expected value of the spin, i.e. the magnetization per spin 
+    fn get_dot_spin_neighbours(&self) -> i32;   // get dot-product of each spin with the sum of it's neighbours
+    fn measure_energy(&self) -> f64;            // get total energy of system
+    fn measure_energy_per_spin(&self) -> f64;   // get energy divided by number of sites
     // IDEAS
     // spacial correlation function
         // correlation with immediate and 2nd degree neighbors... more?
@@ -20,7 +22,7 @@ impl Measurement for Lattice2d {
     /// ∑ s_i
     fn get_spin_sum(&self) -> i32 {
         self.nodes.iter()
-            .fold(0.0 , |acc, &x| acc + x)
+            .fold(0 , |acc, &x| acc + x)
     }
     /// method returns expected value of lattice
     /// ∑ s_i / n
@@ -28,7 +30,7 @@ impl Measurement for Lattice2d {
         self.get_spin_sum() as f64 / (self.n_sites as f64)
     }
     /// method returns dot of spins with their neighbors
-    /// ∑ (s_i * s_j) 
+    /// ∑ (s_i * s_j)   summing over all i,j pairs of neighbors
     fn get_dot_spin_neighbours(&self) -> i32 {
         // circular boudary convolution with neighbor filter
         // 0 1 0
@@ -53,7 +55,7 @@ impl Measurement for Lattice2d {
     }
     /// Returns the energy per spin
     fn measure_energy_per_spin(&self) -> f64 {
-        self.measure_energy() / self.n_sites
+        self.measure_energy() / self.n_sites as f64 
     }
 }
 
@@ -77,7 +79,7 @@ mod test {
         // use std::time::Duration;
         // std::thread::sleep(Duration::from_secs(3));
         for _ in 0..1 { // set 1 to 1000 for slideshow
-            println!("< Spin > = {}", lattice.spin_expected_value());
+            println!("< Spin > = {}", lattice.get_spin_expected_value());
             for _ in 0..1 { // set 1 to 100 for slideshow
                 lattice.update();
             }
