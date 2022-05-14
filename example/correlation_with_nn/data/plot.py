e1dd05d04ccbb69587b5ecd93d019165a6b99bbd
Stephen Fay <dcxstephen@live.co.uk>
2022-05-14 23:25:43 +0100

    python plot of monte carlo simulated data

--- example/correlation_with_nn/data/plot.py
+++ example/correlation_with_nn/data/plot.py
@@ -1,3 +1,4 @@
+# TODO: implement this in rust... eventually
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,12 +6,20 @@ import matplotlib.pyplot as plt
 with open("data.csv","r") as file:
     data = [[float(i) for i in line] for line in csv.reader(file)]
 data = np.asarray(data)
-mu,var,temp = data[:,0],data[:,1],data[:,2]
+temp,ydata = data[:,0],data[:,1:]
+mu = ydata.mean(axis=1)
+# TODO: compute variance
+# var = 
 
+# TODO:make this plot pretty, put it in the readme of this example
+# TODO: first step is to get better time data, better resolution
 plt.figure(figsize=(8,5))
-plt.plot(temp,mu,"r.")
+for idx in range(ydata.shape[1]):
+    plt.plot(temp,ydata[:,idx],"g.")
+plt.plot(temp,mu,"ro")
 # plt.plot(temp,mu,"-",color="black",alpha=0.3,linewidth=0.5)
-plt.errorbar(x=temp,y=mu,xerr=np.zeros(len(temp)),yerr=np.sqrt(var),ecolor="black",elinewidth=0.5)
+# plt.errorbar(x=temp,y=mu,xerr=np.zeros(len(temp)),yerr=np.sqrt(var),ecolor="black",elinewidth=0.5)
+
 
 # x,y = np.arange(10),np.arange(10)
 # plt.errorbar(x=x,y=x,xerr=np.zeros(10),yerr=np.ones(10),fmt="k-",linewidth=0.5,elinewidth=0.5)
