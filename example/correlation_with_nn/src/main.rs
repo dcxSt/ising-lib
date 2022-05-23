5030c3a33cbbefcd3e6cebe193965a4fa5c68a66
Stephen Fay <dcxstephen@live.co.uk>
2022-05-23 02:35:04 +0200

    example, writes output to file

--- example/correlation_with_nn/src/main.rs
+++ example/correlation_with_nn/src/main.rs
@@ -3,23 +3,31 @@ use ising_lib::monte_carlo_measurement::{MonteCarlo, MonteCarloParams};
 // use std::io::Write;
 use std::io;
 use std::error::Error;
-use csv;
+// use csv;
+use std::env;
+use std::fs::File;
+use std::io::Write;
 
 /// Mont Carlo sample of correlation with nearest neighbour for many
 /// temperatures.
 fn main() -> Result<(), Box<dyn Error>> {
+    // TODO: find better way to iter through float temps so we don't have this
+    // hacky scaling by a factor of 10
+    let RANGE_LOW:usize = 5;
+    let RANGE_HIGH:usize = 80;
+    let STEP_BY:usize = 1;
     // let mut mu_vars = vec![];
     let mut samples = vec![];
     println!("Starting simulation");
 
     let params = MonteCarloParams {
-        n_runs: 25,
-        flips_to_skip: 500_000,
+        n_runs: 35,
+        flips_to_skip: 1500_000,
         samples_per_run: 1,
         flips_to_skip_intra_run: 0,
     };
 
-    for temp in tqdm_rs::Tqdm::new((15..=100).step_by(5)) {
+    for temp in tqdm_rs::Tqdm::new((RANGE_LOW..=RANGE_HIGH).step_by(STEP_BY)) {
         print!("T={}, ",temp);
         // beta is 1/T (times boltzman constant, but we're ignoring that)
         let beta: f64 = 10.0 / temp as f64;
@@ -43,20 +51,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         // let mu_var = lattice.sample_average_magnetization(params);
         // mu_vars.push([mu_var[0],mu_var[1],temp as f64])
     }
-    println!("Done");
-    // TODO: write to csv instead of printing
-    // let mut wtr = csv::Writer::from_writer(io::stdout());
-    for (idx,temp) in (15..=100).step_by(5).enumerate() {
-    //     file.write_all(format!("{},{},{}\n",mu_vars[idx][0],mu_vars[idx][1],mu_vars[idx][2]).as_bytes()).expect("Write failed");
-        print!("{},",temp as f64 / 10.0);
-        for i in 0..params.n_runs {
-            print!("{},",samples[idx][i])
+    println!("Done computing. Writing to file...");
+
+    let mut file = File::create("./data.csv").unwrap();
+    for (idx,temp) in (RANGE_LOW..=RANGE_HIGH).step_by(STEP_BY).enumerate() {
+        write!(&mut file, "{},", temp as f64 / 10.0).unwrap();
+        for i in 0..(params.n_runs-1) {
+            write!(&mut file, "{},", samples[idx][i]).unwrap();
         }
-        println!();
-        // wtr.write_record(vec![samples[idx]])?;
+        writeln!(&mut file, "{}", samples[idx][params.n_runs-1]).unwrap();
     }
-    // wtr.flush()?;
-    // println!("Data written to file!");
 
     Ok(())
 }
