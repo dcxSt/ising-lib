3687fb3509bb6f713253d03457daefdfd041d8fb
Stephen Fay <dcxstephen@live.co.uk>
2022-06-01 00:38:57 +0200

    implemented sample energy in parallel

--- src/monte_carlo_measurement.rs
+++ src/monte_carlo_measurement.rs
@@ -1,5 +1,6 @@
 use crate::lattice2d::*;
 use crate::measurement::Measurement; // *;
+use std::thread;
 
 /// Parameters for monte carlo sampling
 pub struct MonteCarloParams {
@@ -14,54 +15,13 @@ pub trait MonteCarlo {
     /// Calculates and returns basic metrics by Monto Carlo sampling
     /// - Energy fluctuations
     /// - Avg Magnetic Susceptibility
+    fn sample_energy_parallel(&mut self, params: &MonteCarloParams) -> Vec<Vec<f64>>;
     fn sample_energy(&mut self, params: &MonteCarloParams) -> Vec<Vec<f64>>;
     fn sample_neighbor_correlations(&mut self, params: &MonteCarloParams) -> Vec<Vec<f64>>;
     fn sample_magnetization(&mut self, params: &MonteCarloParams) -> Vec<Vec<f64>>;
     // TODO: implement below function
     // fn sample_estimate_all_metrics(&self , params:MonteCarloParams) -> Vec;
 }
-
-// DISCUSSION
-// Thought: For temporal correlations, the metric to measure this would be
-// mixing time or something else?
-//
-// (1) The mixing time could be given as an estimate of the time that it takes
-// for the probability that a site changes spin to be 1/2 within that window;
-// kind of like a half-life. I wonder if there are ways to do this analytically,
-// i.e. if you can derive this value from the correlations with immediate neighbors.
-//
-// (2) However we must be careful because this is not the only metric, if instead
-// we measured the half life to be the time it takes for your spin to change
-// *at least* once, this would give us a much longer-time estimate; also that
-// metric makes more sense to me intuitively.
-//
-// (3) A third possible metric could meausre "how much time does it take for the
-// probability of my spin being different from where it started to be 1/4"
-// (will this exponentially decay towards 1/2, certainly that's the assymtote)
-//
-// I think (3) is the most legit option.
-//
-// (3) could be implemented in multiple ways,
-// (3.1) a computationally expensive way
-// could be to measure the correlation (square of diff) of the state at time
-// t(0) with all subsequent times until t(cnst * n), where n is the number of
-// sites. (perhaps this constant cnst could also depend on temperature);
-// then curve fit an exponential decay and voila
-//
-// (3.2) a cheaper way could be to measure only the correlation between the
-// t(0)'th sample and the t(cnst * n * f(temp))'s sample; I think this is the way
-// to go...
-//
-// YOU NEED TO UNDERSTAND HOW CORRELATIONS IN SPACE AND TIME ARE MEASURED
-//
-// We should divide the correlation across time by the size of the lattice
-// because one flip is attempted with every tick i.e. unit of time
-// so larger lattices and graphs will have stronger correlations across time
-// unless we divide by the number of sites.
-//
-// Should the correlations accuracy of correlations in time be handled by the user?
-// Or should it be automatic. I think giving the user too much optionality
-// might make this tool confusing to use...
 
 /// Implements the measurement trait for the Lattice2d type
 impl MonteCarlo for Lattice2d {
@@ -94,7 +54,8 @@ impl MonteCarlo for Lattice2d {
     //     // Return avg magnetization, fluctuations, uncertainty
     // }
     /// Monte Carlo sample of energy
-    /// Returns a vec of energy samples, of length params.n_runs * params.samples_per_run
+    /// Returns a vec of energy samples, of length 
+    /// params.n_runs * params.samples_per_run
     fn sample_energy(&mut self, params: &MonteCarloParams) -> Vec<Vec<f64>> {
         let mut energy = vec![vec![0.0; params.samples_per_run]; params.n_runs];
         for i in 0..params.n_runs {
@@ -109,6 +70,52 @@ impl MonteCarlo for Lattice2d {
         }
         return energy;
     }
+
+    /// Monte Carlo sample of energy in parallel
+    /// Returns a vec of energy samples, of length 
+    /// params.n_runs * params.samples_per_run
+    fn sample_energy_parallel(&mut self, params: &MonteCarloParams) -> Vec<Vec<f64>> {
+        // let mut energy = vec![vec![0.0; params.samples_per_run]; params.n_runs]; // all samples
+        let mut energy = vec![];
+        // let threads: Vec<_> = (0..params.n_runs).map(|i| {
+        //     let mut lattice_clone = self.clone();
+        //     let samples_per_run = params.samples_per_run;
+        //     let flips_to_skip = params.flips_to_skip;
+        //     let flips_to_skip_between_runs = params.flips_to_skip_between_runs;
+        //     thread::spawn(move || {
+        //         for j in 0..samples_per_run {
+        //             
+        //         }
+        //     })
+        // }).collect();
+        let mut fetch_handle = vec![];
+        for i in 0..params.n_runs {
+            // Create a clone: inits new lattice with same input params
+            let mut lattice_copy = self.clone();
+            let flips_to_skip = params.flips_to_skip;
+            let flips_to_skip_between_samples = params.flips_to_skip_between_samples;
+            let samples_per_run = params.samples_per_run;
+            // Time evolve the system to cool (or heat) it
+            fetch_handle.push(thread::spawn(move || -> Vec<f64> {
+                lattice_copy.update_n(flips_to_skip);
+                let mut erg_samples = vec![];
+                for _ in 0..samples_per_run {
+                    // Time evolve the system a bit
+                    lattice_copy.update_n(flips_to_skip_between_samples);
+                    erg_samples.push(lattice_copy.measure_energy());
+                }
+                erg_samples
+            }));
+        }
+        // fetch_handle.into_iter().map(|c| c.join().unwrap());
+
+        for thread in fetch_handle.into_iter() {
+            energy.push(thread.join().unwrap());
+        }
+        return energy;
+    }
+
+
 
     /// Monte Carlo estimate of nearest neighbor correlations
     /// Returns a vec of mean samples, of length params.n_runs
@@ -186,12 +193,77 @@ mod test {
             0.0f64, // h static field term
             beta,   // 1/TkB
         );
+        let energy_samples: Vec<Vec<f64>> = lattice.sample_energy(&params);
+        assert_eq!(energy_samples.len(), params.n_runs);
+        assert_eq!(energy_samples[0].len(), params.samples_per_run);
+    }
 
-        let sample_points: Vec<Vec<f64>> = lattice.sample_energy(&params);
-        assert_eq!(sample_points.len(), params.n_runs);
-        assert_eq!(sample_points[0].len(), params.samples_per_run);
+    #[test]
+    fn test_sample_energy_parallel() {
+        let params = MonteCarloParams {
+            n_runs: 5,
+            flips_to_skip: 1_000,
+            samples_per_run: 10,
+            flips_to_skip_between_samples: 100,
+        };
+        let beta: f64 = 2.4;
+        let mut lattice = Lattice2d::new(
+            [9,9],
+            UpdateRule::Metropolis,
+            SpinType::SpinHalf,
+            InitType::Random,
+            1.0f64, // j interaction constant
+            0.0f64, // h static field term
+            beta,   // 1/TkB
+        );
+        let energy_samples: Vec<Vec<f64>> = lattice.sample_energy_parallel(&params);
+        assert_eq!(energy_samples.len(), params.n_runs);
+        assert_eq!(energy_samples[0].len(), params.samples_per_run);
     }
 }
 
+
+// DISCUSSION
+// Thought: For temporal correlations, the metric to measure this would be
+// mixing time or something else?
+//
+// (1) The mixing time could be given as an estimate of the time that it takes
+// for the probability that a site changes spin to be 1/2 within that window;
+// kind of like a half-life. I wonder if there are ways to do this analytically,
+// i.e. if you can derive this value from the correlations with immediate neighbors.
+//
+// (2) However we must be careful because this is not the only metric, if instead
+// we measured the half life to be the time it takes for your spin to change
+// *at least* once, this would give us a much longer-time estimate; also that
+// metric makes more sense to me intuitively.
+//
+// (3) A third possible metric could meausre "how much time does it take for the
+// probability of my spin being different from where it started to be 1/4"
+// (will this exponentially decay towards 1/2, certainly that's the assymtote)
+//
+// I think (3) is the most legit option.
+//
+// (3) could be implemented in multiple ways,
+// (3.1) a computationally expensive way
+// could be to measure the correlation (square of diff) of the state at time
+// t(0) with all subsequent times until t(cnst * n), where n is the number of
+// sites. (perhaps this constant cnst could also depend on temperature);
+// then curve fit an exponential decay and voila
+//
+// (3.2) a cheaper way could be to measure only the correlation between the
+// t(0)'th sample and the t(cnst * n * f(temp))'s sample; I think this is the way
+// to go...
+//
+// YOU NEED TO UNDERSTAND HOW CORRELATIONS IN SPACE AND TIME ARE MEASURED
+// corr(x,y) = cov(x,y) / sqrt( var(x) var(y) )
+//
+// We should divide the correlation across time by the size of the lattice
+// because one flip is attempted with every tick i.e. unit of time
+// so larger lattices and graphs will have stronger correlations across time
+// unless we divide by the number of sites.
+//
+// Should the correlations accuracy of correlations in time be handled by the user?
+// Or should it be automatic. I think giving the user too much optionality
+// might make this tool confusing to use...
 
 
