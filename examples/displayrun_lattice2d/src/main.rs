476a4975b7e80405f4cb0a21189abb03d40cefa8
Stephen Fay <dcxstephen@live.co.uk>
2022-08-28 22:06:25 -0400

    fixed example

--- examples/displayrun_lattice2d/src/main.rs
+++ examples/displayrun_lattice2d/src/main.rs
@@ -4,7 +4,7 @@ use std::{thread, time};
 fn main() {
     let seven_millis = time::Duration::from_millis(7);  // wait 7 millis to slow simulation
     // let mut lattice = Lattice2d::new_basic([45,145]);   // create a lattice
-    let mut lattice = Lattice2d::new([45,45],
+    let mut lattice = Lattice2d::new([45,125],
                                      UpdateRule::Metropolis,
                                      SpinType::SpinHalf,
                                      InitType::Random,
@@ -12,8 +12,8 @@ fn main() {
                                      0.0f64,
                                      0.20f64);
     lattice.disp_terminal();
-    for _ in 0..1 {                  // 1200 frames 
-        for _ in 0..2_000_000 {
+    for _ in 0..1200 {                  // 1200 frames 
+        for _ in 0..2_000 {
             lattice.update();           // update lattice 2000 times between each frame
         }
         thread::sleep(seven_millis);    // wait 7 millis for smooth flip-card video display
