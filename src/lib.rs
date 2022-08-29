a0e3d70a020173ed9b9a314eb430547d002d95f9
Stephen Fay <dcxstephen@live.co.uk>
2022-08-28 22:39:49 -0400

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! Everything you need to run Ising model simulations on different 
+//! networks (although, so far, only the classic 2d grid ising model 
+//! has been implemented as the Ising2d type). 
+//! Despite its simplicity, the simulation allows us to observe an 
+//! interesting physical phenomenon - phase transition.
+//! Refer to the github repository for [examples](https://github.com/micouy/ising_lib). 
+
 
 pub mod lattice2d;
 pub mod measurement;
